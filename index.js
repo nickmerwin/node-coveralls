be2f7b8407e84bd3db1c3d30b2a0b225c9faaf15
Anna Henningsen <sqrt@entless.org>
2015-04-15 17:11:10 +0200

    Use minimist to parse command line options
    The command line options are made available as a object
    via `index.options`

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+var minimist = require('minimist');
+
+// this needs to go before the other require()s so that
+// the other files can already use index.options
+exports.options = minimist(process.argv.slice(2), {
+  boolean: ['verbose', 'stdout'],
+  alias: { 'v': 'verbose', 's': 'stdout' }
+});
+
 var dir = './lib/';
 exports.convertLcovToCoveralls = require(dir + 'convertLcovToCoveralls');
 exports.sendToCoveralls = require(dir + 'sendToCoveralls');
